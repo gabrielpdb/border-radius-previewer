249111ec7450e420c4b253045f7058257bbf0ae7
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-26 20:58:41 -0300

    everything working now

--- scripts.js
+++ scripts.js
@@ -1,22 +1,35 @@
 const shape = document.querySelector('.shape')
+const copyInput = document.querySelector('#copyInput')
+const inputsNL = document.querySelectorAll('input')
+const inputs = Array.from(inputsNL)
+console.log(inputs[0].value)
 
 function getUnity() {
   return document.querySelector('select#unit').value
 }
 
 function updateValues() {
-  shape.style['border-top-left-radius'] =
-    document.querySelector('input#top-left').value + getUnity()
-  shape.style['border-top-right-radius'] =
-    document.querySelector('input#top-right').value + getUnity()
-  shape.style['border-bottom-left-radius'] =
-    document.querySelector('input#bottom-left').value + getUnity()
-  shape.style['border-bottom-right-radius'] =
-    document.querySelector('input#bottom-right').value + getUnity()
+  shape.style['border-top-left-radius'] = inputs[0].value + getUnity()
+  shape.style['border-top-right-radius'] = inputs[1].value + getUnity()
+  shape.style['border-bottom-left-radius'] = inputs[2].value + getUnity()
+  shape.style['border-bottom-right-radius'] = inputs[3].value + getUnity()
+}
+
+function inputCode() {
+  const code = `
+    border-radius: ${
+      inputs[0].value != 0 ? inputs[0].value + getUnity() + ', ' : '0, '
+    } ${inputs[1].value != 0 ? inputs[1].value + getUnity() + ', ' : '0, '} ${
+    inputs[2].value != 0 ? inputs[2].value + getUnity() + ', ' : '0, '
+  } ${inputs[3].value != 0 ? inputs[3].value + getUnity() + ', ' : '0;'}
+  `
+
+  copyInput.value = code
 }
 
 document.addEventListener('keydown', event => {
   if (event.key == 'Enter') {
     updateValues()
+    inputCode()
   }
 })
